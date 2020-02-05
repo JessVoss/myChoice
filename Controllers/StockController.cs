9b5bf6408564c62fd8288e4f43278c4c18546fea
jvoss18 <voss18@JB202-L02-W-D>
2020-02-05 13:22:10 -0500

    Add project files.

---
+++ Controllers/StockController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myChoice.Models;
+
+namespace myChoice.Controllers
+{
+    public class StockController : Controller
+    {
+        // GET: Stock
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult searchForInstock(string nameOfItem, string department, int numberInStock)
+        {
+            inventory i = new inventory()
+            {
+                nameOfItem = nameOfItem,
+                department = department,
+                numberInStock = numberInStock
+
+            };
+            return View("confirmAmountOfStock", i);
+        }
+        public ActionResult searchForInstock()
+        {
+            return View("newSearchForm");
+        }
+    }
+}
---
+++ Models/inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myChoice.Models
+{
+    public class inventory
+    {
+        // item in question, department it can be found in and the amount in stock
+        public string nameOfItem { get; set; }
+        public string department { get; set; }
+        public int numberInStock { get; set; }
+    }
+}
